59456974994f78bee2dce4dbb21d0edea52cfc35
KnasKnas <christian.rudfoss@gmail.com>
2016-02-24 14:12:43 +0100

    Minor modifications to framework in ComfZone

--- PUPro/src/piCode/ComfZone.java
+++ PUPro/src/piCode/ComfZone.java
@@ -2,13 +2,20 @@ package piCode;
 
 
 public class ComfZone {
+	public double safeDistance; 
 	
 	public void checkZone(){
-		
+		if (okDistance(safeDistance) == true){
+			
+		}
 	}
 	
 	private boolean okDistance(double sensorData){
 		return true; //placeholder
 	}
 
+	public static void main(String[]args){
+		//Main-method
+	}
+	
 }
