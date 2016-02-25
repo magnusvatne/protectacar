4326797aed0ea8a53a823e12f85a99f105d30839
unknown <jrlhlme@hotmail.com>
2016-02-25 10:52:02 +0100

    added skeleton code to comfzone and safedistance

--- PUPro/src/piCode/SafeDistance.java
+++ PUPro/src/piCode/SafeDistance.java
@@ -1,15 +1,22 @@
 package piCode;
 
 public class SafeDistance {
-
-	private int distance; //type not decided
 	
-	public void checkAhead(){
-		
+	
+	// should change UI to reflect something
+	public boolean checkAhead(double sensorData, double carSpeed){
+		return okDistance(sensorData, carSpeed);		
 	}
 	
-	private boolean okDistance(){
-		return true;
+	// sensorData is distance to car in front
+	private boolean okDistance(double sensorData, double carSpeed){
+		
+		double carTravelDistance = (carSpeed / 3.6) * 3;
+		
+		if (sensorData >= carTravelDistance) {
+			return true;
+		}
+		return false;
 	}
 
 }
