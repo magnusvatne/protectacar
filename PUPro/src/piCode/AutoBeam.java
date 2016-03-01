2b23cd887600aab69b46fcb5397e2e4735f460b3
Christian Rudfoss Haugland <Christian Rudfoss Haugland>
2016-03-01 12:41:19 +0100

    Added the folder monitoring the cameraoutput. Also made changes to AutoBeam

--- PUPro/src/piCode/AutoBeam.java
+++ PUPro/src/piCode/AutoBeam.java
@@ -1,19 +1,39 @@
 package piCode;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
+import java.util.EventListener;
+import java.util.Scanner;
+
+import javafx.beans.value.ObservableValue;
+
+import javax.swing.event.ChangeEvent;
 //import java.util.EventListener;
 
 public class AutoBeam {
 	boolean on;
 	boolean lightSwitch = false;
+	boolean cameraOutput;
+	
+	BufferedReader reader = null;
+	
+	public ChangeListener cameraChange = new ChangeListener<ChangeListener>();
+	
+	
 	
 	public AutoBeam(double sensorData){
 		boolean threshold = checkThreshold(sensorData);
-		while (on==true){
+		while (on==true && cameraOutput == true){
 			if (threshold){
 				switchBeam();
 			}
@@ -40,17 +60,35 @@ public class AutoBeam {
 		}
 		return false;
 	}
-}
+	
+	void addChangeListener(ChangeListener changedCameraOutput){
+		
+	}
 
 public static void main (String args[]){
+	File cameraFile = new File("cameraoutput.txt");
+	
 	// create gpio controller
-    final GpioController gpio = GpioFactory.getInstance();
+    //final GpioController gpio = GpioFactory.getInstance();
     
     // provision gpio pin #01 as an output pin and turn on
-    final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+    //final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
 
     // set shutdown state for this pin
-    pin.setShutdownOptions(true, PinState.LOW);
+    //pin.setShutdownOptions(true, PinState.LOW);
+    
+    try{
+    	Scanner input = new Scanner(cameraFile);
+    	while (input.hasNext()){
+    		String cameraValue = input.nextLine();
+    		System.out.println("CameraFile has been read.");
+    	}
+    }
+    
+    catch(FileNotFoundException e){
+    	System.err.format("File does not exist");
+    }
+	}
     
    
 }
