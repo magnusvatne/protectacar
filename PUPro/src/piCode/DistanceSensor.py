13e78f9b3337272403c0e20136c2dcdde36a8423
Bortne <kristsbo@stud.ntnu.no>
2016-03-03 09:41:59 +0000

    Added python script for distancesensor and safedistance

---
+++ PUPro/src/piCode/DistanceSensor.py
@@ -0,0 +1,197 @@
+
+import RPi.GPIO as GPIO
+import time
+from collections import deque
+import statistics
+GPIO.setmode(GPIO.BCM)
+
+TRIG_BAK_VENSTRE	 = 4
+TRIG_BAK_HOGRE		 = 20
+TRIG_FRAMME_VENSTRE	 = 16
+TRIG_FRAMME_HOGRE	 = 26
+
+
+ECHO_BAK_VENSTRE	 = 27 #Connect ECHO to Pin 18 on the Raspberry Pi
+ECHO_BAK_HOGRE		 = 22 #Connect ECHO to Pin 40 on the Raspberry Pi
+ECHO_FRAMME_VENSTRE	 = 23 #Connect ECHO to Pin 37 on the Raspberry Pi
+ECHO_FRAMME_HOGRE	 = 24 #Connect ECHO to Pin 15 on the Raspberry Pi
+
+
+print ("Distance Measurement In Progress")
+#Setup av signal
+GPIO.setup(TRIG_BAK_VENSTRE, GPIO.OUT)
+GPIO.setup(TRIG_BAK_HOGRE, GPIO.OUT)
+GPIO.setup(TRIG_FRAMME_VENSTRE, GPIO.OUT)
+GPIO.setup(TRIG_FRAMME_HOGRE, GPIO.OUT)
+
+
+GPIO.setup(ECHO_BAK_VENSTRE,GPIO.IN)
+GPIO.setup(ECHO_BAK_HOGRE,GPIO.IN)
+GPIO.setup(ECHO_FRAMME_VENSTRE,GPIO.IN)
+GPIO.setup(ECHO_FRAMME_HOGRE,GPIO.IN)
+
+GPIO.output(TRIG_BAK_VENSTRE, False)
+GPIO.output(TRIG_BAK_HOGRE, False)
+GPIO.output(TRIG_FRAMME_VENSTRE, False)
+GPIO.output(TRIG_FRAMME_HOGRE, False)
+
+avstand1 = deque()
+avstand2 = deque()
+avstand3 = deque()
+avstand4 = deque()
+
+print ("Done setting up, lets go!")
+
+def bak_venstre():
+	i = 1
+	distance = 1
+	while(i <= 3):
+		#time.sleep()
+		GPIO.output(TRIG_BAK_VENSTRE, True)
+		time.sleep(0.00001)
+		GPIO.output(TRIG_BAK_VENSTRE, False)
+			
+		timeoutTime = time.time()
+		while GPIO.input(ECHO_BAK_VENSTRE) == 0:
+			pulse_start = time.time()
+			if pulse_start - timeoutTime > 3:
+				break	
+
+		timeoutTime = time.time()
+		while GPIO.input(ECHO_BAK_VENSTRE) == 1:
+			pulse_end = time.time()
+			if pulse_end - timeoutTime > 3:
+				break
+
+		duration = pulse_end - pulse_start
+		distance = duration * 17150
+		distance = round(distance,2)
+	
+		avstand1.append(distance)
+		if len(avstand1) >= 10:
+			avstand1.popleft()
+		distance = round(statistics.median(avstand1),2)
+
+		#print (distance, avstand1)
+		i += 1
+	print("Bak venstre",distance)
+
+
+def framme_venstre():
+	i = 1
+	distance = 1
+	while(i <= 3):
+                #time.sleep()
+		GPIO.output(TRIG_FRAMME_VENSTRE, True)
+		time.sleep(0.00001)
+		GPIO.output(TRIG_FRAMME_VENSTRE, False)
+		
+		timeoutTime = time.time()
+		while GPIO.input(ECHO_FRAMME_VENSTRE) == 0:
+			pulse_start = time.time()
+			if pulse_start - timeoutTime > 3:
+				break
+		
+		timeoutTime = time.time()
+		while GPIO.input(ECHO_FRAMME_VENSTRE) == 1:
+			pulse_end = time.time()
+			if pulse_end - timeoutTime > 3:
+				break
+
+
+		duration = pulse_end - pulse_start
+		distance = duration * 17150
+		distance = round(distance,2)
+
+		avstand2.append(distance)
+		if len(avstand2) >= 10:
+			avstand2.popleft()
+		distance = round(statistics.median(avstand2),2)
+
+                #print (distance, avstand1)
+		i += 1
+	print("Framme venstre",distance)
+
+
+def bak_hogre():
+	i = 1
+	distance = 0
+	while(i <= 3):
+		GPIO.output(TRIG_BAK_HOGRE, True)
+		time.sleep(0.00001)
+		GPIO.output(TRIG_BAK_HOGRE, False)
+
+		timeoutTime = time.time()
+		while GPIO.input(ECHO_BAK_HOGRE) == 0:
+			pulse_start = time.time()
+			if pulse_start - timeoutTime > 3:
+				break
+
+		while GPIO.input(ECHO_BAK_HOGRE) == 1:
+			pulse_end = time.time()
+			if pulse_end - timeoutTime > 3:
+				break
+
+
+		duration = pulse_end - pulse_start
+		distance = duration * 17150
+		distance = round(distance,2)
+
+		avstand3.append(distance)
+		if len(avstand3) >= 10:
+			avstand3.popleft()
+		distance = round(statistics.median(avstand3),2)
+
+                #print (distance, avstand1)
+		i += 1
+	print("Bak hogre", distance)
+
+
+def framme_hogre():
+	i = 1
+	distance = 0
+	while(i <= 3):
+		GPIO.output(TRIG_FRAMME_HOGRE, True)
+		time.sleep(0.00001)
+		GPIO.output(TRIG_FRAMME_HOGRE, False)
+		
+		timeoutTime = time.time()
+		while GPIO.input(ECHO_FRAMME_HOGRE) == 0:
+			pulse_start = time.time()
+			if pulse_start - timeoutTime > 3:
+				break
+		
+		timeoutTime = time.time()
+		while GPIO.input(ECHO_FRAMME_HOGRE) == 1:
+			pulse_end = time.time()
+			if pulse_end - timeoutTime > 3:
+				break
+
+		duration = pulse_end - pulse_start
+		distance = duration * 17150
+		distance = round(distance,2)
+
+		avstand4.append(distance)
+		if len(avstand4) >= 10:
+			avstand4.popleft()
+		distance = round(statistics.median(avstand4),2)
+
+                #print (distance, avstand1)
+		i += 1
+	print("Framme hogre", distance)
+
+
+
+
+def main():
+	while(True):
+		time.sleep(1)
+		bak_venstre()
+		framme_venstre()
+		bak_hogre()
+		framme_hogre()
+		print("")
+		print("-----------------")
+		print("")
+
+main()
---
+++ PUPro/src/piCode/SafeDistance.py
@@ -0,0 +1,56 @@
+import RPi.GPIO as GPIO
+import time
+from collections import deque
+import statistics
+GPIO.setmode(GPIO.BCM)
+
+TRIG	 = 5
+ECHO	 = 6
+
+
+print ("Distance Measurement In Progress")
+#Setup av signal
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+GPIO.output(TRIG, False)
+
+avstand = deque()
+
+print ("Done setting up, lets go!")
+
+def safeDistance():
+	#time.sleep()
+	GPIO.output(TRIG, True)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, False)
+			
+	timeoutTime = time.time()
+	while GPIO.input(ECHO) == 0:
+		pulse_start = time.time()
+		if pulse_start - timeoutTime > 3:
+			break	
+
+	timeoutTime = time.time()
+	while GPIO.input(ECHO) == 1:
+		pulse_end = time.time()
+		if pulse_end - timeoutTime > 3:
+			break
+
+	duration = pulse_end - pulse_start
+	distance = duration * 17150
+	distance = round(distance,2)
+	
+	avstand.append(distance)
+	if len(avstand) >= 20:
+		avstand.popleft()
+	distance = round(statistics.median(avstand),2)
+
+	#print (distance, avstand1)
+	print("Avstand til kjøretøy foran: ",distance)
+
+
+def main():
+	while(True):
+		time.sleep(0.2)
+		safeDistance()
+main()
