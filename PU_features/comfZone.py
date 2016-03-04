f3b3a169f37b4238508d03635bc2dab9f3f307f7
unknown <jrlhlme@hotmail.com>
2016-03-04 11:23:31 +0100

    added code for ComfZone

---
+++ PU_features/comfZone.py
@@ -0,0 +1,33 @@
+import sensorAccess
+
+class ComfZone:
+    sensorDataLeft = sensorAccess.get() #placeholder function, should get data
+    sensorDataRight = sensorAccess.get()
+    isOn = false #whether ComfZone is turned on by user
+    leftPin,rightPin #for giving feedback on whether a car is in your blindzone
+
+    safeDistance = #final num to compare with distance from sensor
+
+    def checkZone():
+        while (isON):
+            sensorDataLeft = sensorAccess.get()
+            sensorDataRight = sensorAccess.get()
+
+            if (check(sensorDataLeft)):
+                leftPin.toggleOn
+            else:
+                leftPin.toggleOff
+
+            if (check(sensorDataRight)):
+                rightPin.toggleOn
+            else:
+                rightPin.toggleOff
+
+
+    def check(sensorData):
+        if (sensorData < safeDistance):
+            return true
+        return false
+
+    def setOn(on):
+        self.isOn = on
