527f4530b381582a6320da131ecf56df3c4794f2
KnasKnas <christian.rudfoss@gmail.com>
2016-03-07 11:28:22 +0100

    Accomodate for sensors valuating in cm in safeDistance.py

--- PU_features/safeDistance.py
+++ PU_features/safeDistance.py
@@ -5,6 +5,7 @@ class safeDistance:
         return okDistance(sensorData,carSpeed)
 
     def okDistance(sensorData, carSpeed):
+    	sensorData = sensorData/100 #Divided by 100 to accomodate for the sensor monitoring in cm
         carTravelDistance = (carSpeed/3.6)*3 #distance travelled in 3 seconds
         if (sensorData < carTravelDistance):
             return false
