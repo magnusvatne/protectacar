644f77a0f62f2ed6a71df41e17deb793ff482651
Christian Haugland <christian.rudfoss@gmail.com>
2016-03-14 15:02:46 +0100

    Added a program(not yet finished) that gets the current speed of the car

---
+++ chr/speedMonitor.py
@@ -0,0 +1,13 @@
+
+
+class main():
+
+	#This method needs to instantly get the current speed of the vehicle via the
+	#OBD-II plug from the PI to the car.
+	def getCurrentSpeed():
+		pass
+
+	def main():
+		getCurrentSpeed():
+
+main()
