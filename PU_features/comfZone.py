b8aec57c245c176805fc4a3b0a5892703a4a88c3
unknown <jrlhlme@hotmail.com>
2016-03-14 15:13:53 +0100

    safeDistance and comfZone now uses functions imported from DistanceSensor.

--- PU_features/comfZone.py
+++ PU_features/comfZone.py
@@ -1,20 +1,24 @@
-import sensorAccess
+import DistanceSensor
 
 class ComfZone():
-    sensorDataLeft = sensorAccess.get() #placeholder function, should get data
-    sensorDataRight = sensorAccess.get()
+    #Deprecated
+    #sensorDataLeft = sensorAccess.get() #placeholder function, should get data
+    #sensorDataRight = sensorAccess.get()
+    #/Deprecated
+
     isOn = False #whether ComfZone is turned on by user
     leftPin,rightPin #for giving feedback on whether a car is in your blindzone
 
     safeDistance = 8#final num to compare with distance from sensor
 
-    def checkZone():
-        while (isON):
-            sensorDataLeft = sensorAccess.get()
-            sensorDataRight = sensorAccess.get()
+    def checkZone(): #runs in background, will check zones when isOn = true
+        while (isOn):
+            sensorDataLeft = DistanceSensor.bak_hogre()
+            sensorDataRight = DistanceSensor.bak_venstre()
 
+            #Must implement pin control, unfinished as is
             if (check(sensorDataLeft)):
-                leftPin.toggleOn
+                leftPin.toggleOn 
             else:
                 leftPin.toggleOff
 
@@ -29,5 +33,13 @@ class ComfZone():
             return true
         return false
 
-    def setOn(on):
-        self.isOn = on
+
+    #setter and getter for isOn
+    
+    def toggleOn(): 
+        if (self.isOn)
+            self.isOn = False
+        self.isOn = True
+
+    def checkOn():
+        return self.isOn
--- PU_features/safeDistance.py
+++ PU_features/safeDistance.py
@@ -1,13 +1,18 @@
-import sensorAccess
+import DistanceSensor
 
 class safeDistance:
 
-	def checkAhead(sensorData, carSpeed):
-	    return okDistance(sensorData,carSpeed)
+        carSpeed = 60 #Placeholder until car speed can be extracted
+
+	def checkAhead():
+            distance = DistanceSensor.safeDistance()
+            if (distance < 0)
+                    return False #returns false if sensor is broken
+	    return okDistance(distance,carSpeed) 
 
 	def okDistance(sensorData, carSpeed):
-		sensorData = sensorData/100 #Divided by 100 to accomodate for the sensor monitoring in cm
+            sensorData = sensorData/100 #Divided by 100 to accomodate for the sensor monitoring in cm
 	    carTravelDistance = (carSpeed/3.6)*3 #distance travelled in 3 seconds
 	    if (sensorData < carTravelDistance):
-	        return false
-	    return true
+	        return False
+	    return True
